26389cde67c162a4b841b9b8b3d9edcbd3fd7e41
OscarZhou <OscarZhou@users.noreply.github.com>
2017-05-25 09:55:26 +1200

    add decoding code
    emulator two squres to decode successfully.

--- color2dbarcodes/main.cpp
+++ color2dbarcodes/main.cpp
@@ -23,6 +23,43 @@ struct s_block
     int r;
 };
 
+struct s_color
+{
+    bool b;
+    bool g;
+    bool r;
+
+};
+
+/*                       black      red       green      blue       white        cyan         magenta     yellow*/
+//static s_block colors[8]={{0,0,0}, {180,0,0}, {0,180,0},{0,0,180},{180,180,180},{0,180,180},{180,0,180},{180,180,0}};
+
+static s_color b_colors[24] = {{false, false, false},
+                        {true, true, true},
+                        {false, false, true},
+                        {true, true, true},
+                        {false, true, false},
+                        {true, true, true},
+                        {false, true, true},
+                        {true, true, true},
+                        {true, false, false},
+                        {true, true, true},
+                        {true, false, true},
+                        {true, true, true},
+                        {true, true, false},
+                        {true, true, true},
+                        {false, false, false},
+                        {true, true, true},
+                        {false, false, true},
+                        {true, true, true},
+                        {false, true, false},
+                        {true, true, true},
+                        {false, true, true},
+                        {true, true, true},
+                        {true, false, false},
+                        {true, true, true}};
+
+static int numofblockinrow[24] = {4, 8, 10, 12, 13, 15, 16, 17, 18, 18, 19, 20, 20, 21, 21, 22, 22, 22, 22, 23, 23, 23, 23, 23};
 
 int main(int argc, char** argv)
 {
@@ -207,12 +244,125 @@ int main(int argc, char** argv)
     rotmatrix = getRotationMatrix2D(Point(center.x, center.y), angle, 1);
     warpAffine(affineImg, affineImg, rotmatrix, colorImg.size());
 
-    /**/
 
+
+    /* align the center of the circle */
+    int icol = 0;//center.x-30;
+    //int irow = 0;//center.y-30;
+    //int k;
+    //for(k=center.x-30; k<center.x+30; k++)
+    //{
+    //    s_block
+    //}
+
+    /*find the blocks contains the valid information */
+
+    /*
+    int irow;
+    int offset[23] = {0}; // the value 20 is not fixed
+
+    for(irow=0; irow<23; irow++)
+    {
+        offset[irow] = 15;
+    }
+
+    Point tmpCenter = Point(center.x, center.y-offset[22]);  // initial value is the neighbor of the center
+    for(irow=22; irow>=0; irow--)
+    {
+        s_block tmpPt[3];
+        bool bflag = true;
+        int j=0;
+        //tmpPt[0] stands for middle line, tmpPt[1] stands for top line, tmpPt[2] stands for bottom line
+        int tmpoffset[3] = {0, -1, 1};
+        for(j=0; j<3; j++)
+        {
+            tmpPt[j].r = MpixelR(affineImg, tmpCenter.x, tmpCenter.y + tmpoffset[j]);
+            tmpPt[j].g = MpixelG(affineImg, tmpCenter.x, tmpCenter.y + tmpoffset[j]);
+            tmpPt[j].b = MpixelB(affineImg, tmpCenter.x, tmpCenter.y + tmpoffset[j]);
+            bool br = (tmpPt[j].r >= 180);
+            bool bg = (tmpPt[j].g >= 180);
+            bool bb = (tmpPt[j].b >= 180);
+
+
+            if((br == b_colors[irow].r && bg == b_colors[irow].g && bb == b_colors[irow].b))
+            {
+                offset[irow] = offset[irow] + 15;
+                break;
+            }
+            if((br == b_colors[irow-1].r && bg == b_colors[irow-1].g && bb == b_colors[irow-1].b))
+            {
+
+                if(j == 1 && !bflag)
+                {
+                    offset[irow-1] = offset[irow] - 15;
+                    break;
+                }
+                else if(j == 2 && !bflag)
+                {
+                    offset[irow-1] = offset[irow] + 15;
+                    break;
+                }
+                else
+                {
+                    offset[irow-1] = offset[irow-1] + offset[irow];
+                    bflag = false;
+                }
+            }
+        }
+
+        tmpCenter = Point(center.x, center.y-offset[irow-1]);
+
+        MpixelR(affineImg, tmpCenter.x, tmpCenter.y) = 0;
+        MpixelG(affineImg, tmpCenter.x, tmpCenter.y) = 0;
+        MpixelB(affineImg, tmpCenter.x, tmpCenter.y) = 0;
+
+
+    }
+
+    int loop=0;
+    for(loop=0; loop<24; loop++)
+    {
+        cout<<"row["<<loop<<"]="<<offset[loop]<<endl;
+    }
+
+
+    for(irow=0; irow<23; irow++)
+    {
+
+    }
+    */
+
+    /* decode  emulator 2 square */
+    Point pt1 = Point(center.x-20, center.y);//magenta
+    Point pt2 = Point(center.x+10, center.y);//red
+    s_block b1, b2;
+    b1.r = MpixelR(affineImg, pt1.x, pt1.y)>180 ?1:0;
+    b1.g = MpixelG(affineImg, pt1.x, pt1.y)>180 ?1:0;
+    b1.b = MpixelB(affineImg, pt1.x, pt1.y)>180 ?1:0;
+
+    b2.r = MpixelR(affineImg, pt2.x, pt2.y)>180 ?1:0;
+    b2.g = MpixelG(affineImg, pt2.x, pt2.y)>180 ?1:0;
+    b2.b = MpixelB(affineImg, pt2.x, pt2.y)>180 ?1:0;
+    cout<<b1.r<<", "<<b1.g<<", "<<b1.b<<endl;
+    int mask = 1<<5 | 1<<4 | 1<<3 | 1<<2 | 1<<1 | 1<<0;
+    int bb1 = (b1.r<<5 | b1.g << 4| b1.b<<3) & mask;
+    int bb2 = (b2.r<<2 | b2.g << 1| b2.b<<0) & mask;
+    cout<<bb1<<endl;
+    char x = encodingarray[bb1 | bb2];
+    cout<<"x= "<<x<<endl;
+
+    MpixelR(affineImg, pt1.x, pt1.y) = 0;
+    MpixelG(affineImg, pt1.x, pt1.y) = 0;
+    MpixelB(affineImg, pt1.x, pt1.y) = 0;
+
+
+    MpixelR(affineImg, pt2.x, pt2.y) = 0;
+    MpixelG(affineImg, pt2.x, pt2.y) = 0;
+    MpixelB(affineImg, pt2.x, pt2.y) = 0;
 
     //namedWindow("Before Rotation", WINDOW_NORMAL );
     //imshow("Before Rotation", colorImg);
-    namedWindow("After Rotation", WINDOW_AUTOSIZE  );
+    namedWindow("After Rotation", WINDOW_AUTOSIZE);
     imshow("After Rotation", affineImg);
 
     //cvNamedWindow("Source", WINDOW_AUTOSIZE   );
@@ -232,3 +382,6 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+
+
